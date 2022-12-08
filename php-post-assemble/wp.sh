959adbefd8a61d5d4ae805b1d4af14daab053b07
Antti Koskinen <koskinenaa@users.noreply.github.com>
2022-12-08 12:25:49 +0200

    Update wp.sh

--- php-post-assemble/wp.sh
+++ php-post-assemble/wp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ls -la /opt/app-root/src/vendor/wp-cli
+ls -la /opt/app-root/src/vendor
 # bash /opt/app-root/src/vendor/wp-cli/wp-cli/bin/wp --version
 
 # kun wp-config.php ja tietokanta ovat asetettu
