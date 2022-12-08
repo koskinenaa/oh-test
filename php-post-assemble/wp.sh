bd8b3be7240ff5f041de7555813b8307d6c75d52
Antti Koskinen <koskinenaa@users.noreply.github.com>
2022-12-08 12:11:54 +0200

    Update wp.sh

--- php-post-assemble/wp.sh
+++ php-post-assemble/wp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wp --version
+bash /opt/app-root/src/vendor/wp-cli/wp-cli/bin/wp --version
 
 # kun wp-config.php ja tietokanta ovat asetettu
 # bash wp plugin activate $(bash wp plugin list --status=inactive --field=name)
