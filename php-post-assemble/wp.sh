88a85f5f4844deadc18fc029503e7ccae6279b65
Antti Koskinen <koskinenaa@users.noreply.github.com>
2022-12-15 11:25:08 +0200

    Update wp.sh

--- php-post-assemble/wp.sh
+++ php-post-assemble/wp.sh
@@ -4,6 +4,8 @@
 # ls -la /opt/app-root/src/vendor
 # bash /opt/app-root/src/vendor/wp-cli/wp-cli/bin/wp --version
 
+./composer.phar config repositories.wpackagist composer https://wpackagist.org
+
 ./composer.phar config --json extra '"installer-paths": {"wp-content/plugins/{$name}": ["type:wordpress-plugin"], "wp-content/mu-plugins/{$name}": ["type:wordpress-muplugin"], "wp-content/themes/{$name}": ["type:wordpress-theme"]}'
 
 ./composer.phar config --no-plugins allow-plugins.composer/installers true
