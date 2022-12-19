57dda49f5efa6d7b2df33a36bc1086dd8e3a8528
Antti Koskinen <koskinenaa@users.noreply.github.com>
2022-12-19 14:34:26 +0200

    updates

--- php-post-assemble/wp.sh
+++ php-post-assemble/wp.sh
@@ -4,7 +4,7 @@
 # ls -la /opt/app-root/src/vendor
 # bash /opt/app-root/src/vendor/wp-cli/wp-cli/bin/wp --version
 
-./composer.phar require ${INSTALLABLE_PACKAGES}
+# ./composer.phar require ${INSTALLABLE_PACKAGES}
 
 # kun wp-config.php ja tietokanta ovat asetettu
 # bash wp plugin activate $(bash wp plugin list --status=inactive --field=name)
