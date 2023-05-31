c43b6bf90bb5bf483e21d42ce8b3fce381ae781a
Antti Koskinen <koskinenaa@users.noreply.github.com>
2023-05-31 23:07:12 +0300

    Update install-mysql.sh

--- php-post-assemble/install-mysql.sh
+++ php-post-assemble/install-mysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dnf install -y https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm
+# dnf install -y https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm
 
-dnf install -y mysql-community-client
+# dnf install -y mysql-community-client
