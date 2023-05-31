25d716ea656997c87e322d97b69662fc19d79d90
Antti Koskinen <koskinenaa@users.noreply.github.com>
2023-05-31 23:28:31 +0300

    edit

--- php-post-assemble/install-wp-cli.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-# curl -o /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x /usr/local/bin/wp
--- php-post-assemble/wp.sh
+++ php-post-assemble/wp.sh
@@ -7,8 +7,3 @@
 # ./composer.phar require ${INSTALLABLE_PACKAGES}
 
 # kun wp-config.php ja tietokanta ovat asetettu
-# which wp
-
-# bash wp db create
-
-# bash wp plugin activate $(bash wp plugin list --status=inactive --field=name)
