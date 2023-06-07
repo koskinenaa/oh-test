4322c8b4dbb81f6d0c912f84aea7cc5a473eecf5
Antti Koskinen <koskinenaa@users.noreply.github.com>
2023-06-07 14:56:11 +0300

    Delete wp.sh

--- php-post-assemble/wp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# ls -la /opt/app-root/src/vendor/wp-cli
-# ls -la /opt/app-root/src/vendor
-# bash /opt/app-root/src/vendor/wp-cli/wp-cli/bin/wp --version
-
-# ./composer.phar require ${INSTALLABLE_PACKAGES}
-
-# kun wp-config.php ja tietokanta ovat asetettu
